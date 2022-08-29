d11dd2d6eac303aad0900b94800cf08f9d4205b5
Prasoonjha1 <57556301+Prasoonjha1@users.noreply.github.com>
2022-08-29 10:23:10 +0530

    Update Info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -4,11 +4,11 @@ function Info({user})
         <header className="heaader">
             <h4 className="name">{user.name}</h4>
             <h4 className="tech">{user.profession}</h4>
-            <h4 className="webs" href = {'http://'+user.web}>{user.web}</h4>
-            <a href={user.link.email}>
+            <h4 className="webs"><a href={'http://'+user.web}>{user.web}</a></h4>
+            <a href={'http://'+user.link.email}>
                 <button className="b1" ><i class="bi bi-envelope-fill icon"> Email</i></button>
             </a>
-            <a href={user.link.linkedin}>
+            <a href={'http://'+user.link.linkedin}>
                 <button className="b2"><i class="bi bi-linkedin icon1"> Linkedin</i></button>
             </a>
         </header>
